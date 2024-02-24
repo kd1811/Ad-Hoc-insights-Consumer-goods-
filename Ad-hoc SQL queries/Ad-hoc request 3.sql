7c13f38ea87441598d27221faeffac335bf6dd8c
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:13:44 +0530

    Ad-hoc request 3.sql

--- Ad-hoc SQL queries/Ad-hoc request 3.sql
+++ Ad-hoc SQL queries/Ad-hoc request 3.sql
@@ -1,3 +1,7 @@
+/* Provide a report with all the unique product counts for each segment and sort them in descending order of product counts. 
+The final output contains 2 fields,
+- segment
+- product_count */
 SELECT
 segment,
 COUNT(DISTINCT(product_code)) AS product_count
