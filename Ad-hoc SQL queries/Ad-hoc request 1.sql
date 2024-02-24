9df33fca72ff0cef90df8985a39b67c4b1af5275
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:10:44 +0530

    Ad-hoc request 1.sql

--- Ad-hoc SQL queries/Ad-hoc request 1.sql
+++ Ad-hoc SQL queries/Ad-hoc request 1.sql
@@ -1,3 +1,5 @@
+/*Provide the list of markets in which customer "Atliq Exclusive" operates its business in the APAC region*/
+  
 SELECT market FROM dim_customer
 WHERE customer = "AtliQ Exclusive" 
 AND region = "APAC";
