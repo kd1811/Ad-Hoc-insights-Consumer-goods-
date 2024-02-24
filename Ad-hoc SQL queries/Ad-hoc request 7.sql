95f0dd6d9ea08c80d92747e4f38801fa64e2f6c4
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:18:05 +0530

    Ad-hoc request 7.sql

--- Ad-hoc SQL queries/Ad-hoc request 7.sql
+++ Ad-hoc SQL queries/Ad-hoc request 7.sql
@@ -1,3 +1,9 @@
+/* Get the complete report of the Gross sales amount for the customer “Atliq Exclusive” for each month. This analysis helps to get an idea of low and high-performing months and take strategic decisions.
+The final report contains these columns:
+- Month
+- Year
+- Gross sales Amount */
+
 SELECT
 MONTHNAME(s.date) AS Month,
 YEAR(s.date) AS Year,
