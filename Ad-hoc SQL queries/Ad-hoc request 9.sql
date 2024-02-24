f23abe3fc372a76984a1582673778daf19aefbdd
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:19:28 +0530

    Ad-hoc request 9.sql

--- Ad-hoc SQL queries/Ad-hoc request 9.sql
+++ Ad-hoc SQL queries/Ad-hoc request 9.sql
@@ -1,3 +1,9 @@
+/* Which channel helped to bring more gross sales in the fiscal year 2021 and the percentage of contribution?
+The final output contains these fields,
+- channel
+- gross_sales_mln
+- percentage */
+
 WITH CTE1 AS
 (
 SELECT
