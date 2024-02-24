e1ecc3ea086eec4566778f2066e84ccae955eeda
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:16:17 +0530

    Ad-hoc request 5.sql

--- Ad-hoc SQL queries/Ad-hoc request 5.sql
+++ Ad-hoc SQL queries/Ad-hoc request 5.sql
@@ -1,3 +1,9 @@
+/* Get the products that have the highest and lowest manufacturing costs.
+The final output should contain these fields,
+- product_code
+- product
+- manufacturing_cost */
+
 #Products that have the highest and lowest manufacturing costs
 
 SELECT 
