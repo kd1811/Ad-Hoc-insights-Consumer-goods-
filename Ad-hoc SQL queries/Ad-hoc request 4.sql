4f2677dfc5eca89997fe49362c24ddabd41b3e17
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:15:18 +0530

    Ad-hoc request 4.sql

--- Ad-hoc SQL queries/Ad-hoc request 4.sql
+++ Ad-hoc SQL queries/Ad-hoc request 4.sql
@@ -1,3 +1,10 @@
+/* Which segment had the most increase in unique products in 2021 vs 2020?
+The final output contains these fields,
+- segment
+- product_count_2020
+- product_count_2021
+- difference */
+
 WITH a AS
 			(SELECT
 			p.segment,
