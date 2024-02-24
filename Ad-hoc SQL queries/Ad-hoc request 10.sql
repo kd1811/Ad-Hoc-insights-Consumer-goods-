1ad9ff57500686ee4f6ea7547b885c7fd2fc3309
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:20:02 +0530

    Ad-hoc request 10.sql

--- Ad-hoc SQL queries/Ad-hoc request 10.sql
+++ Ad-hoc SQL queries/Ad-hoc request 10.sql
@@ -1,7 +1,10 @@
 /* Get the Top 3 products in each division that have a high total_sold_quantity in the fiscal_year 2021?
 The final output contains these fields,
 - division
-- product_code */
+- product_code
+- product
+- total_sold_quantity
+- rank_order */
 
 WITH CTE1 AS
 		  (
