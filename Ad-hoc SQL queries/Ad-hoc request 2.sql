e55015070b8f56135a9647516924f4b053adbebe
Kandarp Desai <144077186+kd1811@users.noreply.github.com>
2024-02-24 13:12:59 +0530

    Ad-hoc request 2.sql

--- Ad-hoc SQL queries/Ad-hoc request 2.sql
+++ Ad-hoc SQL queries/Ad-hoc request 2.sql
@@ -1,3 +1,9 @@
+/* What is the percentage of unique product increase in 2021 vs. 2020? 
+The final output contains these fields,
+- unique_products_2020
+- unique_products_2021
+- percentage_chg */
+
 WITH CTE1 AS
 (SELECT 
 COUNT(DISTINCT(product_code)) AS unique_products_2020
